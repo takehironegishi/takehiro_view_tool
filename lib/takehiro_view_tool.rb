f0489611fd249d36f7f35d469361063ef49dd22a
takehironegishi <takehiro.n61@gmail.com>
2021-08-29 16:19:04 +0900

    initial commit

---
+++ lib/takehiro_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "takehiro_view_tool/version"
+require_relative "takehiro_view_tool/renderer"
+
+module TakehiroViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
